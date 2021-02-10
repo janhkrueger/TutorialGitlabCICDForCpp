5dd7f61ee688db71e28b89dee46696f2077f15f9
janhkrueger <janhkrueger@outlook.com>
2021-02-10 01:12:11 +0100

    Initial source commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+
+//============================================================================
+// Name        : main.cpp
+// Author      : JHK
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, tutorial for Gitlab CI/CD Pipeline
+//============================================================================
+
+#include <string>
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
